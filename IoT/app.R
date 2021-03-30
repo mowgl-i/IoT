62fe56285585db37e21dd6e69c4fb8701eb47353
mowgli <46568362+mowgl-i@users.noreply.github.com>
2021-03-29 20:26:05 -0400

    finished

--- IoT/app.R
+++ IoT/app.R
@@ -6,6 +6,7 @@ library(reactable)
 #install.packages("googlesheets")
 library(googlesheets4)
 library(lubridate)
+library(ggrepel)
 
 
 
@@ -36,38 +37,73 @@ getData <- function () {
 }
 
 getData()
-
+sleep_data <- read_csv("C:/Users/macia/Documents/MSIA-21/IoT/Amazon Health Data/Sleep/Sleep_Sessions_ddf09bef-af8c-4a5b-bbd9-6bed8faff4b3.csv")
+sleep_dates <- lubridate::date(unique(sleep_data$`Date Of Sleep`))
+dates<-lubridate::date(unique(Data$Day))
+skin_temp <- read_csv("C:/Users/macia/Documents/MSIA-21/IoT/Amazon Health Data/Sleep/Skin_Temperature_13f1f3d5-f722-4ea5-89ea-78929a307892.csv")
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-    
-    # Application title
-    titlePanel("Mowgli at his desk"),
-    
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            radioButtons('Day_select','Which Day would you like to view?',c('2021-03-29',
-                                                                     '2021-03-28',
-                                                                     '2021-03-27',
-                                                                     '2021-03-26',
-                                                                     '2021-03-25',
-                                                                     '2021-03-24',
-                                                                     '2021-03-23')),
-            
+# ui <- fluidPage(
+#     
+#     # Application title
+#     titlePanel("Mowgli at his desk"),
+#     
+#     # Sidebar with a slider input for number of bins
+#     sidebarLayout(
+#         sidebarPanel(
+#             radioButtons('Day_select','Which Day would you like to view?',dates),
+#             
+# 
+#         ),
+#         
+#         # Show a plot of the generated distribution
+#         mainPanel(
+#             plotOutput('Time_plot')
+#         )
+#     )
+# )
 
-        ),
+
+ui <- fluidPage(
+    navlistPanel(
+        id = "tabset",
+        "IoT Reactive: Measure of Screen Time",
+        tabPanel("Time at desk",
+                 mainPanel(
+                 radioButtons('Day_select','Which Day would you like to view?',dates),
+                 plotOutput('Time_plot')
+                 )
+                 ),
+        "IoT Reactive: Sleep Quality",
+         tabPanel("Time Asleep",mainPanel(
+            radioButtons('Sleep_select','Which Day would you like to view?',sleep_dates),
+            plotOutput('Sleep_Breakdown')
+             
+             
+             
+         )),
         
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput('Time_plot')
-        )
+        tabPanel("Overall Skin Temperature",
+                 mainPanel(
+                     plotOutput('Skin_temp')
+                 ))
     )
 )
+
+
+
+
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
     filter_data <- reactive({Data %>% mutate(Day = as.factor(Day)) %>% filter(Day == input$Day_select)})
+    
+    filter_sleep <- reactive({
+        sleep_data %>% filter(`Date Of Sleep` == input$Sleep_select) %>% select(`Rem Sleep Percentage`,`Light Sleep Percentage`,`Deep Sleep Precentage`) %>% 
+            gather(key = 'Sleep_Type',value = 'Value')
+    })
+    
     
     output$Time_plot <- renderPlot({
         ggplot(filter_data(),aes(x = as_datetime(Daytime), y = as.numeric(Value), color = Outcome))+
@@ -76,9 +112,38 @@ server <- function(input, output) {
             xlab('Light Res')})
     
     
+    output$Sleep_Breakdown <- renderPlot({
+        
+        ggplot(filter_sleep(),aes(x = Sleep_Type,y=Value,fill=Sleep_Type,label = paste(round(Value),'%')))+
+            geom_bar(stat='identity',show.legend = F)+
+            coord_polar()+
+            theme_minimal()+
+            geom_label_repel()+
+            xlab(NULL)+
+            ylab(NULL)+
+            theme(axis.text.y = element_blank(),
+                  axis.text.x = element_blank(),
+                  legend.title = element_blank(),
+                  title = element_text(face ="bold"))+ 
+            labs(title= 'Sleep Breakdown')})
+    
+    output$Skin_temp <- renderPlot({
+        ggplot(skin_temp,aes(x = `time(utc)`,y = `Skin Temperature (Celsius)`))+geom_line()+geom_smooth()+labs(title = 'Skin temp through the day')+theme_minimal()
+        
+    })
+    
+    
 }
+
+
+
 
 # Run the application
 shinyApp(ui = ui, server = server)
+
+
+
+
+#sleep_data %>% filter(`Date Of Sleep` == sleep_dates[4])
 
 
